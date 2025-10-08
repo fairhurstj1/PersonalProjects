5e2d090bce0cbab579814f6e7ab63a29566d2043
Jonathan Fairhurst <fairhurstj1@gmail.com>
2025-10-08 18:14:31 -0400

    Created basic calculator

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,6 +7,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("hello world");
+            const double length1 = 24;
+            const double length2 = 14;
+            const double width = 1.75;
+            double area1 = (area(length1, width) * 2) + (area(length2, width) * 2);
+            Console.WriteLine(area1);
         }
+
+        static double area(double width, double height)
+        {
+            return width * height;
+        }
+
+ 
+
     }
 }
