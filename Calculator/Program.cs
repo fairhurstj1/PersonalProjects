1106150b7ed426b101e2c4c180ffffc5fa2ae48f
Jonathan Fairhurst <fairhurstj1@gmail.com>
2025-10-08 18:08:27 -0400

    Initialized repository, started new project, tested terminal

---
+++ Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Calculator
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
